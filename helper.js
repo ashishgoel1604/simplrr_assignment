b33b5cab0555940c84a56e1678a25ba253d007cc
Ashish Goel <ashishgoel@Ashishs-MacBook-Air.local>
2020-09-27 19:34:46 +0530

    created helper file, to keep common codes

---
+++ helper.js
@@ -0,0 +1,36 @@
+const https = require('http');
+const baseUrl = 'https://api.yelp.com/';
+//Assuming that, i have to make only one type of request, so hardcoding headers
+const headers = {
+    'Authorization': 'Bearer pSxxXBpunBnBT0mIoP_m6VTEy32nmGTOG8xO5_9ehd8uIPpSgZvaSTxqmVoNdR_OxuXI2u5JLB5gltsjBUxat5sy4ci96wvRmWJT_Bmw473B48rMEeZQml2lcPBeX3Yx'
+}
+
+//The requests we need to make are both GET requests, so making this function only
+const makeHttpGetRequest = function (url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, { headers: headers }, response => {
+            let jsonResponse = '';
+            response.on('data', chunk => {
+                jsonResponse += chunk;
+            });
+            response.on('error', err => {
+                reject(err);
+            });
+            response.on('end', () => {
+                //request completed
+                //parsing json in try catch
+                try {
+                    const parsedResponse = JSON.parse(jsonResponse);
+                    resolve(parsedResponse);
+                } catch (err) {
+                    reject(err);
+                }
+            });
+        });
+    });
+}
+
+module.exports = {
+    baseUrl,
+    makeHttpGetRequest
+}

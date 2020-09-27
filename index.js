dc7aea0f326809ae8a1aab776e6c481eff3f8904
Ashish Goel <ashishgoel@Ashishs-MacBook-Air.local>
2020-09-27 20:43:43 +0530

    used promise all function to make requests parallely, and used for loop to make requests in parts

--- index.js
+++ index.js
@@ -2,6 +2,35 @@ const helper = require('./helper');
 //Assuming that, i have to make only one type of request, with fixed filters, so hardcoding url
 const businessSearchUrl = `${helper.baseUrl}v3/businesses/search?location=Redwood%20City&sort_by=rating&limit=10`;
 
+const getAddress = function (location) {
+    if (location) {
+        let address = location.address1 ? location.address1 : (location.address2 ? location.address2 : (location.address3 ? location.address3 : 'NA')) + location.city ? `, ${location.city}` : '';
+        return address;
+    } else {
+        return 'NA';
+    }
+}
+
+//this function will return array of final response objects by merging two arrays different arrays of api response
+const createResponseObjects = function (businesses, reviews) {
+    const results = [];
+    //we can use any length, bcz both will have same length
+    for (let i = 0; i < businesses.length; i++) {
+        const business = businesses[i];
+        const review = reviews[i];
+        const firstReview = review.reviews[0];
+        const output = {
+            businessName: business.name,
+            businessAddress: getAddress(business.location),
+            customerReview: firstReview.text,
+            customer: firstReview.user.name,
+            rating: business.rating
+        };
+        results.push(output);
+    }
+    return results;
+}
+
 //this function will return the final response
 const getTopTenIceCreamShops = async function () {
     let finalResults = [];
@@ -9,12 +38,24 @@ const getTopTenIceCreamShops = async function () {
         //first getting top 10 businesses.
         const result = await helper.makeHttpGetRequest(businessSearchUrl);
         if (result && result.businesses && result.businesses.length) {
-            //for each business id, getting its reviews. [this api have a limit for request per second, so making requests one by one
-            //(i could have used Promise.all, but to keep the code straight, i am calling apis one by one)]
-            for (business of result.businesses) {
-                const reviewUrl = `https://api.yelp.com/v3/businesses/${business.businessId}/reviews`;
-                const reviewData = await helper.makeHttpGetRequest(reviewUrl);
+            //Now for each business id, getting its reviews. [this api have a limit for request per second, 
+            //so making requests in parts
+            const promises = [];
+            const selectedBusinesses = [];
+            for (let i = 0; i < result.businesses.length; i++) {
+                const reviewUrl = `https://api.yelp.com/v3/businesses/${result.businesses[i].id}/reviews`;
+                promises.push(helper.makeHttpGetRequest(reviewUrl));
+                selectedBusinesses.push(result.businesses[i]);
+                if (promises.length === 5 || i === result.businesses.length - 1) {
+                    const reviewsData = await Promise.all(promises);// it maintains the order
+                    const resultsPart = createResponseObjects(selectedBusinesses, reviewsData);
+                    finalResults = [...finalResults, ...resultsPart];
+                    selectedBusinesses.length = 0;
+                    promises.length = 0;
+                }
             }
+            console.log(JSON.stringify(finalResults));
+            return finalResults;
         } else {
             throw new Error('No data found');
         }
@@ -22,5 +63,6 @@ const getTopTenIceCreamShops = async function () {
         console.log(err);
         return null;
     }
-
 }
+
+getTopTenIceCreamShops();
